23732a03c51972f3ec6c3f04455cca7acefa0a71
geltapatio <luismiguel.gamiz@gmail.com>
2022-05-14 13:22:28 +0200

    Entity Framework nuget package was added DbContext was adde General and Custom Repositories were added Perrsistence and Infrastructure projects were added

--- GlobalTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GlobalTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -10,13 +12,15 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
     {
         private readonly IEventRepository _eventRepository;
         private readonly IMapper _mapper;
+        private readonly IEmailService _emailService;
         private readonly ILogger<CreateEventCommandHandler> _logger;
 
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, ILogger<CreateEventCommandHandler> logger)
+        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService, ILogger<CreateEventCommandHandler> logger)
         {
             _mapper = mapper;
             _eventRepository = eventRepository;
+            _emailService = emailService;
             _logger = logger;
         }
 
@@ -32,6 +36,18 @@ namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.Crea
 
             @event = await _eventRepository.AddAsync(@event);
 
+            //Sending email notification to admin address
+            Email? email = new() { To = "gill@snowball.be", Body = $"A new event was created: {request}", Subject = "A new event was created" };
+
+            try
+            {
+                await _emailService.SendEmail(email);
+            }
+            catch (Exception ex)
+            {
+                //this shouldn't stop the API from doing else so this can be logged
+                _logger.LogError($"Mailing about event {@event.EventId} failed due to an error with the mail service: {ex.Message}");
+            }
 
             return @event.EventId;
         }
