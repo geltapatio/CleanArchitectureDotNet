815740385615dbeb9943f5b075211c0b3224cb38
geltapatio <luismiguel.gamiz@gmail.com>
2022-05-16 19:40:53 +0200

    - WebApi project was adde - Swagger was added

--- GlobalTicket.TicketManagement.Api/Program.cs
+++ GlobalTicket.TicketManagement.Api/Program.cs
@@ -1,3 +1,4 @@
+using GlobalTicket.TicketManagement.Api.OeprationFilter;
 using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
@@ -9,11 +10,19 @@ builder.Services.AddApplicationServices();
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddPersistenceServices(builder.Configuration);
 builder.Services.AddControllers();
-builder.Services.AddCors(options => options.AddPolicy("Open", policy=> policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
+builder.Services.AddCors(options => options.AddPolicy("Open", policy => policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod()));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Version = "v1",
+        Title = "GloboTicket Ticket Management API"
+    });
+    c.OperationFilter<FileResultContentTypeOperationFilter>();
+}
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,7 +34,12 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
-app.UseCors();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+});
+app.UseCors("Open");
 
 app.UseAuthorization();
 
---
+++ GlobalTicket.TicketManagement.Infrastructure/FileExport/CsvExporter.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Features.Events.Queries.GetEventsExport;
+
+namespace GloboTicket.TicketManagement.Infrastructure
+{
+    public class CsvExporter : ICsvExporter
+    {
+        public byte[] ExportEventsToCsv(List<EventExportDto> eventExportDtos)
+        {
+            using MemoryStream? memoryStream = new MemoryStream();
+            using (StreamWriter? streamWriter = new StreamWriter(memoryStream))
+            {
+                using CsvWriter? csvWriter = new CsvWriter(streamWriter, null, false);
+                csvWriter.WriteRecords(eventExportDtos);
+            }
+
+            return memoryStream.ToArray();
+        }
+    }
+}
